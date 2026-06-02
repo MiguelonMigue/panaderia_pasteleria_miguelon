983d86f9ea23b5279f2443ab0644b19aba81a67d
miguelonmiguelon <miguelcatholic@gmail.com>
2026-06-01 21:12:14 -0500

    subiendo los postres

--- panaderia_pasteleria_miguelon/src/components/About.jsx
+++ panaderia_pasteleria_miguelon/src/components/About.jsx
@@ -1,13 +1,33 @@
-import Header from "./Header"
+import Header from "./Header";
+import Footer from './Footer'
+import comiendo_onces from '../assets/tomando_onces.png'
 
 const About = () => {
+  const caracteristicas = [
+    {
+      id: 1,
+      titulo: "Sobre nosotros",
+      paragraph:
+        "Somos un salón de onces, en donde el cliente podrá disfrutar de deliciosos desayunos, almuerzos, platos especiales, onces y bebidas calientes. Un lugar ideal para invitar a la familia y a los amigos, ah, y también a los novios",
+      img: comiendo_onces
+    },
+  ];
   return (
     <div>
-      <Header/>
-      <h1>Sobre nosotros</h1>
-      <p>Somos un salón de onces, en donde el cliente podrá disfrutar de deliciosos desayunos, almuerzos, platos especiales, onces y bebidas calientes. Un lugar ideal para invitar a la familia y a los amigos, ah, y también a los novios</p>
-    </div>
-  )
-}
 
-export default About
+      <Header />
+      <div className="galeria">
+        {caracteristicas.map((caracteristica)=>(
+          <div className="foto">
+            <h1 className="titulo_principal">{caracteristica.titulo}</h1>
+            <p>{caracteristica.paragraph}</p>
+            <img src={caracteristica.img} width='250px' height='243px' alt="" />
+          </div>
+        ))}
+      </div>
+      <Footer/>
+    </div>
+  );
+};
+
+export default About;
--- panaderia_pasteleria_miguelon/src/components/Contact.jsx
+++ panaderia_pasteleria_miguelon/src/components/Contact.jsx
@@ -1,11 +1,16 @@
 import Header from "./Header"
-
+import Footer from "./Footer"
+import tomando_onces from '../assets/tomando_onces.png'
 const Contact = () => {
   return (
     <div>
       <Header/>
+      <div className="foto">
       <h1>Miguel Buitrago, gerente</h1>
       <h3>WhatsApp: +57 313 355 0351</h3>
+      <img src={tomando_onces} alt="" width='400px' height='300px'/>
+      </div>
+      <Footer/>
     </div>
   )
 }
