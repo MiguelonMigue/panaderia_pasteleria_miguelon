22784cbfb5f0540c211a83a7add6e7190cf3e51e
miguelonmiguelon <miguelcatholic@gmail.com>
2026-09-07 22:20:12 -0500

    subiendo cambios de react con carousel

--- panaderia_pasteleria_miguelon/src/components/Alimentos.jsx
+++ panaderia_pasteleria_miguelon/src/components/Alimentos.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { AlimentosContext } from "../context/AlimentosContext";
 import Header from "./Header";
 import Footer from "./Footer";
+import {Carousel} from 'react-bootstrap'
 
 const Alimentos = () => {
   const { alimentos, loading } = useContext(AlimentosContext);
@@ -12,30 +13,30 @@ const Alimentos = () => {
 
   return (
     <div>
-      <Header />
-      <div className="container mt-4">
-        <h2 className="text-center mb-4">Nuestros Alimentos</h2>
-        <div className="row">
+     
+      <Carousel>
+        
           {alimentos && alimentos.map((item) => (
-            <div key={item.id || item._id} className="col-md-4 mb-4">
-              <div className="card h-100">
-                {/* Etiqueta de imagen corregida sin variables de estilo propensas a error */}
+            <Carousel.Item key={item.id || item._id} >
+          
                 <img 
                   src={item.imagen || item.img || "https://via.placeholder.com/300"} 
                   alt={item.nombre || "Alimento"} 
                   className="card-img-top"
                   style={{ height: "200px", objectFit: "cover" }}
-                />
-                <div className="card-body">
+                  />
+                  <Carousel.Caption>
+              <h2 className="text-center mb-4">Nuestros Alimentos</h2>
                   <h5 className="card-title">{item.nombre}</h5>
                   <p className="card-text">{item.descripcion}</p>
-                </div>
-              </div>
-            </div>
+          </Carousel.Caption>
+              
+            </Carousel.Item>
           ))}
-        </div>
-      </div>
-      <Footer />
+
+      </Carousel>
+      
+      
     </div>
   );
 };
